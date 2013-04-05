9554069a3ca27f7f675dc8468e77c5ea692182ac
loganlinn <logan.linn@gmail.com>
2013-04-04 23:06:53 -0700

    Notes on events

---
+++ src/powergrid/events.clj
@@ -0,0 +1,27 @@
+(ns powergrid.events)
+
+;;;; Incoming Message Types
+;; register
+;; set-color
+;; set-region
+;; ready
+;; buy-power-plant
+;; bid-power-plant
+;; buy-cities
+;; buy-resources
+;; spend-resources
+;; move-resources
+;; trash-power-plant
+;; pass
+;; end-move
+;; status
+;;
+;;;; Output Message Types
+;; game-begin
+;; game-end
+;; update-resources
+;; update-power-plants
+;; update-money
+;; update-players
+;;
+;; status
