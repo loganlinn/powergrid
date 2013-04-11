fbf442ce008dfed159b0189efe652267895d064d
loganlinn <logan.linn@gmail.com>
2013-04-10 18:05:52 -0700

    Rename state -> game, because that's what it is.

--- src/powergrid/core.clj
+++ src/powergrid/core.clj
@@ -11,13 +11,13 @@
 
 (defn max-network-size
   "Returns the maximum number of cities a single player has built"
-  [state]
-  (apply max (map network-size (players state))))
+  [game]
+  (apply max (map network-size (players game))))
 
 (defn update-player
-  "Returns state after updating player with f"
-  [state player-key f & args]
-  (apply update-in state [:players player-key] f args))
+  "Returns game after updating player with f"
+  [game player-key f & args]
+  (apply update-in game [:players player-key] f args))
 
 (defn network-size
   "Returns the number of cities in the player's network"
@@ -41,9 +41,9 @@
   (assoc player :money (+ (:money player 0) amt)))
 
 (defn purchase
-  "Returns state after transferring amt Elektro from player to bank"
-  [state player-key price]
-  (-> state
+  "Returns game after transferring amt Elektro from player to bank"
+  [game player-key price]
+  (-> game
       (update-player player-key update-money (- price))
       (update-in [:bank] (fnil + 0) price)))
 
@@ -132,9 +132,9 @@
                    players))))
 
 (defn update-player-order
-  "Returns state after updating player order"
-  [state]
-  (update-in state [:players] player-order))
+  "Returns game after updating player order"
+  [game]
+  (update-in game [:players] player-order))
 
 (defn power-plant-order
   "Returns power-plants after re-ordering"
@@ -147,51 +147,51 @@
            :future (concat future step-3-card))))
 
 (defn update-power-plant-order
-  "Returns state after ordering the power-plants"
-  [{:keys [step power-plants] :as state}]
-  (update-in state [:power-plants] power-plant-order state))
+  "Returns game after ordering the power-plants"
+  [{:keys [step power-plants] :as game}]
+  (update-in game [:power-plants] power-plant-order game))
 
 (defn remove-power-plant
-  "Returns state after removing power-plant from the current power-plant market"
-  ([state power-plant market]
-   (update-in state [:power-plants market] (partial remove #(= % power-plant))))
-  ([state power-plant]
-   (remove-power-plant state power-plant :market)))
+  "Returns game after removing power-plant from the current power-plant market"
+  ([game power-plant market]
+   (update-in game [:power-plants market] (partial remove #(= % power-plant))))
+  ([game power-plant]
+   (remove-power-plant game power-plant :market)))
 
 (defn drop-lowest-power-plant
   "Removes lowest power-plant from market. Assumes power-plant market is in
   order. Note, no replacement is drawn."
-  [state]
-  (update-in state [:power-plants :market] rest))
+  [game]
+  (update-in game [:power-plants :market] rest))
 
 (defn add-to-power-plant-market
-  "Returns state after adding power-plant to the power plant market and
+  "Returns game after adding power-plant to the power plant market and
   re-ordering"
-  [state power-plant]
-  (-> state
+  [game power-plant]
+  (-> game
       (update-in [:power-plants :future] conj power-plant)
       (update-power-plant-order)))
 
 (defn handle-step-3-card
-  "Returns state after handling the Step 3 card"
-  [{:keys [phase] :as state} step-3-card]
-  (let [state (-> state
-                  (update-in [:power-plants :deck] shuffle)
-                  (assoc :step-3-card? true))]
+  "Returns game after handling the Step 3 card"
+  [{:keys [phase] :as game} step-3-card]
+  (let [game (-> game
+                 (update-in [:power-plants :deck] shuffle)
+                 (assoc :step-3-card? true))]
     (if (= phase 2)
       (add-to-power-plant-market step-3-card)
-      (update-power-plant-order (drop-lowest-power-plant state)))))
+      (update-power-plant-order (drop-lowest-power-plant game)))))
 
 (defn draw-power-plant
-  "Returns state after moving card from power-plant deck to market and
+  "Returns game after moving card from power-plant deck to market and
   re-ordering"
-  [state]
-  (let [[draw & deck] (get-in state [:power-plants :deck])]
+  [game]
+  (let [[draw & deck] (get-in game [:power-plants :deck])]
     (if (step-3-card? draw)
-      (-> state
+      (-> game
           (assoc-in [:power-plants :deck] deck)
           (handle-step-3-card draw))
-      (-> state
+      (-> game
           (assoc-in [:power-plants :deck] deck)
           (add-to-power-plant-market draw)))))
 
@@ -209,98 +209,98 @@
 (defmulti step-complete? :step)
 (defmulti do-phase :phase)
 
-(defmethod prep-phase :default [state] state)
-(defmethod post-phase :default [state] state)
-(defmethod prep-step :default [state] state)
-(defmethod post-step :default [state] state)
-(defmethod phase-complete? :default [state] (turns-remain? state))
-(defmethod step-complete? :default [state] false)
+(defmethod prep-phase :default [game] game)
+(defmethod post-phase :default [game] game)
+(defmethod prep-step :default [game] game)
+(defmethod post-step :default [game] game)
+(defmethod phase-complete? :default [game] (turns-remain? game))
+(defmethod step-complete? :default [game] false)
 
-(defmethod prep-phase 1 [state]
-  (assoc state :turns []))
+(defmethod prep-phase 1 [game]
+  (assoc game :turns []))
 
-(defmethod prep-phase 2 [state]
-  (assoc state :turns (reset-turns (num-players state) false)))
+(defmethod prep-phase 2 [game]
+  (assoc game :turns (reset-turns (num-players game) false)))
 
 (defn post-phase-2-step-3-card
-  [state]
-  (-> state
+  [game]
+  (-> game
       (remove-power-plant (step-3-card) :future)
       (drop-lowest-power-plant)))
 
-(defmethod post-phase 2 [{:keys [round step-3-card?] :as state}]
-  (cond-> state
+(defmethod post-phase 2 [{:keys [round step-3-card?] :as game}]
+  (cond-> game
     step-3-card? (post-phase-2-step-3-card)
     (= round 1) (update-player-order)))
 
-(defmethod prep-phase 3 [state]
-  (assoc state :turns (reset-turns (num-players state) true)))
+(defmethod prep-phase 3 [game]
+  (assoc game :turns (reset-turns (num-players game) true)))
 
-(defmethod prep-phase 4 [state]
-  (assoc state :turns (reset-turns (num-players state) true)))
+(defmethod prep-phase 4 [game]
+  (assoc game :turns (reset-turns (num-players game) true)))
 
-(defmethod post-phase 5 [state]
-  (inc-round state))
+(defmethod post-phase 5 [game]
+  (inc-round game))
 
-(defmethod prep-step 2 [state]
-  (-> state
+(defmethod prep-step 2 [game]
+  (-> game
       (drop-lowest-power-plant)
       (draw-power-plant)))
 
-(defmethod prep-step 3 [state]
-  (-> state
+(defmethod prep-step 3 [game]
+  (-> game
       (dissoc :step-3-card?)
       (update-power-plant-order)))
 
-(defmethod step-complete? 1 [state]
-  (and (= (:phase state) 4)
-       (not (turns-remain? state))
-       (>= (max-network-size state)
-           (num-cities-trigger-step-2 (num-players state)))))
+(defmethod step-complete? 1 [game]
+  (and (= (:phase game) 4)
+       (not (turns-remain? game))
+       (>= (max-network-size game)
+           (num-cities-trigger-step-2 (num-players game)))))
 
-(defmethod step-complete? 2 [state]
-  (:step-3-card? state false))
+(defmethod step-complete? 2 [game]
+  (:step-3-card? game false))
 
 (defn game-over?
   "Returns true if conditions have been to end the game, otherwise false"
-  [state]
-  (>= (max-network-size state)
-      (num-cities-trigger-end (num-players state))))
+  [game]
+  (>= (max-network-size game)
+      (num-cities-trigger-end (num-players game))))
 
 (defn tick
-  [state]
-  (if (game-over? state)
-    state
-    (if (step-complete? state)
-      (recur (-> state post-step inc-step prep-step))
-      (if (phase-complete? state)
-        (recur (-> state post-phase inc-phase prep-phase))
-        (do-phase state)))))
+  [game]
+  (if (game-over? game)
+    game
+    (if (step-complete? game)
+      (recur (-> game post-step inc-step prep-step))
+      (if (phase-complete? game)
+        (recur (-> game post-phase inc-phase prep-phase))
+        (do-phase game)))))
 
 ;; PHASE 1
 
 (defmethod do-phase 1
-  [state]
-  (update-player-order state))
+  [game]
+  (update-player-order game))
 
 ;; PHASE 2
 
 (defmethod do-phase 2
-  [state]
+  [game]
   )
 
 ;; PHASE 3
 
 
 (defn update-resource
-  "Returns state after updating resource by applying f, args"
-  [state resource f & args]
+  "Returns game after updating resource by applying f, args"
+  [game resource f & args]
   (apply update-in [:resources resource] f args))
 
 (defn get-resource
-  "Returns the current state of resource"
-  [state resource]
-  (get-in state [:resources resource]))
+  "Returns the current game of resource"
+  [game resource]
+  (get-in game [:resources resource]))
 
 (defmulti accept-resource
   "Returns trader after storing the resource in dest.
@@ -349,21 +349,21 @@
   (update-in resource [dest] (fnil - 0) amt))
 
 (defn purchase-resources
-  "Returns state after processing player's purchases"
-  [state player-key purchases]
+  "Returns game after processing player's purchases"
+  [game player-key purchases]
   (reduce
-    (fn [state [resource amt]]
-      (let [price (resource-price (get-resource state resource) amt)]
-        (-> state
+    (fn [game [resource amt]]
+      (let [price (resource-price (get-resource game resource) amt)]
+        (-> game
             (update-resource resource send-resource :market (- amt))
             (update-player player-key accept-resource resource amt)
             (purchase player-key price))))
-    state
+    game
     purchases))
 
 
 (defmethod do-phase 3
-  [state]
+  [game]
   ;; use purchase-resources
   )
 
@@ -407,31 +407,30 @@
            {} (get resource-table num-players))))
 
 (defn resupply-resources
-  "Returns state after resupplying the resource market according to rules."
-  [{:keys [step] :as state}]
-  (let [rate (resupply-rate (num-players state) step (resource-supply state))]
+  "Returns game after resupplying the resource market according to rules."
+  [{:keys [step] :as game}]
+  (let [rate (resupply-rate (num-players game) step (resource-supply game))]
     ;; Subtract from supply, add to market
     ))
 
 ;; =====================
 
-;(let [state (new-game [(new-player 1 nil :blue) (new-player 2 nil :black)])
-      ;plant1 {:number 36, :resource :coal, :capacity 3, :yield 7}
-      ;plant2 {:number 17, :resource :uranium, :capacity 1, :yield 2}
-      ;plant3 {:number 12, :resource #{:coal :oil}, :capacity 2, :yield 2}
-      ;[p1 p2] (players state)
-      ;state (-> state
-                ;(update-player p1 add-power-plant plant1)
-                ;(update-player p1 add-power-plant plant3)
-                ;(update-player p1 assign-resource plant1 :coal 5)
-                ;(update-player p1 assign-resource plant3 :coal 1)
-                ;;(update-player p1 assign-resource plant3 :oil 1)
-                ;(update-player p2 add-power-plant plant2)
-                ;(update-player p2 add-city :norfolk))
-      ;[p1 p2] (players state)]
-  ;(pprint state)
-  ;(pprint (get-in state [:power-plants]))
-  ;(pprint p1)
-  ;(pprint (resource-capacities p1))
-  ;)
+#_(let [game (new-game [(new-player 1 nil :blue) (new-player 2 nil :black)])
+        plant1 {:number 36, :resource :coal, :capacity 3, :yield 7}
+        plant2 {:number 17, :resource :uranium, :capacity 1, :yield 2}
+        plant3 {:number 12, :resource #{:coal :oil}, :capacity 2, :yield 2}
+        [p1 p2] (players game)
+        game (-> game
+                 (update-player p1 add-power-plant plant1)
+                 (update-player p1 add-power-plant plant3)
+                 (update-player p1 assign-resource plant1 :coal 5)
+                 (update-player p1 assign-resource plant3 :coal 1)
+                 ;(update-player p1 assign-resource plant3 :oil 1)
+                 (update-player p2 add-power-plant plant2)
+                 (update-player p2 add-city :norfolk))
+        [p1 p2] (players game)]
+    (pprint game)
+    (pprint (get-in game [:power-plants]))
+    (pprint p1)
+    (pprint (resource-capacities p1)))
 
