a4c3c677fb8d334cdefe8159d4ceea38e6b7e7f7
loganlinn <logan.linn@gmail.com>
2013-04-15 18:06:06 -0700

    Upgrade to 1.5.1

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.0"]
+  :dependencies [[org.clojure/clojure "1.5.1"]
                  [slingshot "0.10.3"]
                  [io.pedestal/pedestal.service "0.1.2"]
                  [io.pedestal/pedestal.jetty "0.1.2"]
