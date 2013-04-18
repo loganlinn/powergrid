c55b9bfdc370347f8ee48019ebbec75de175675e
loganlinn <logan.linn@gmail.com>
2013-04-18 12:18:09 -0700

    Finish implementing phase3

--- src/powergrid/messages/phase3.clj
+++ src/powergrid/messages/phase3.clj
@@ -4,31 +4,44 @@
             [powergrid.player :as p]
             [powergrid.resource :as r]))
 
+(defn resource-price
+  "Returns the current price for n-units of resource"
+  [game resource n]
+  (-> (g/resource game resource) (r/resource-price n)))
 
 (defn total-price
   [game resources]
-  (apply + (for [[r n] resources]
-             (-> (g/resource game r) (r/resource-price n)))))
+  (reduce (fn [tot [r n]]
+            (+ tot (resource-price game r n)))
+          0
+          resources))
+
+(defn buy-resource
+  "Updates and returns game after buying n-units of resource"
+  [game player-id resource n]
+  (-> game
+      (g/update-resource resource r/send-resource :market n)
+      (g/update-player player-id r/accept-resource resource n)
+      (g/purchase player-id (resource-price game resource n))))
 
 (defrecord BuyResourcesMessage [player-id resources]
   Validated
   (validate [this game]
     (let [player (g/player player-id)]
-     (cond
-      (empty? resources) "Invalid resources specified"
-      (every? r/types (keys resources)) "Invalid resources specified"
-      (every? pos? (vals resources)) "Invalid resource amount"
-      (= player-id (g/current-turn game)) "Not your turn"
-      (not (p/has-capacity? player resources)) "Insufficient power-plant capacity"
-      (not (g/contains-resource? game resources)) "Insufficient resources in market"
-      (p/can-afford? player (total-price game resources)) "Insufficient funds")))
+      (cond
+        (empty? resources) "Invalid resources specified"
+        (every? r/types (keys resources)) "Invalid resources specified"
+        (every? pos? (vals resources)) "Invalid resource amount"
+        (= player-id (g/current-turn game)) "Not your turn"
+        (not (p/has-capacity? player resources)) "Insufficient power-plant capacity"
+        (not (g/contains-resource? game resources)) "Insufficient resources in market"
+        (p/can-afford? player (total-price game resources)) "Insufficient funds")))
 
   GameUpdate
   (update-game [this game]
-    ;; remove from market
-    ;; send to player
-    ;; deduct money from player
-    game)
+    (reduce (fn [game [r n]] (buy-resource game player-id r n))
+            game
+            resources))
 
   Passable
   (passable? [_ _] true)
