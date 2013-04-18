9fe1ec93d33c343a13445d7f70ce8a5acedf06e7
loganlinn <logan.linn@gmail.com>
2013-04-17 18:48:25 -0700

    Add ability to set min-bid when creating auction

--- src/powergrid/auction.clj
+++ src/powergrid/auction.clj
@@ -4,15 +4,18 @@
 (defrecord Auction [item player-id bidders price min-increment])
 
 (def ^:private defaults
-  {:min-increment 1
-   :price 0})
+  {:min-increment 1})
 
 (defn new-auction
   "Returns a new auction"
-  [m]
-  (let [auction (map->Auction (merge defaults m))
-        bidders (:bidders auction )]
-    (assoc auction :bidders (queue bidders))))
+  ([m]
+   {:pre [(contains? m :item)]}
+   (let [auction (map->Auction (merge defaults m))
+         bidders (:bidders auction )]
+     (assoc auction :bidders (queue bidders))))
+  ([m initial-min-bid]
+   (let [auction (new-auction m)]
+     (assoc auction :price (- initial-min-bid (:min-increment auction))))))
 
 (defn current-bidder
   "Returns the current bidder"
@@ -22,7 +25,9 @@
 (defn min-bid
   "Returns the minimum bid for item currently at auction"
   [{:keys [price min-increment]}]
-  (+ price min-increment))
+  (if price
+    (+ price min-increment)
+    0))
 
 (defn completed?
   "Returns true if the auction is in a completed state, otherwise false"
--- test/powergrid/auction_test.clj
+++ test/powergrid/auction_test.clj
@@ -2,15 +2,17 @@
   (:require [powergrid.auction :refer :all]
             [midje.sweet :refer :all]))
 
-(fact new-auction "defaults"
-  (let [a (new-auction {:player-id 3 :bidders [1 2 3] :price 200})]
-    (instance? clojure.lang.PersistentQueue (:bidders a))
-    (= (conj clojure.lang.PersistentQueue/EMPTY 1 2 3) (:bidders a))
-    (number? (:price a)) => true
-    (number? (:min-increment a)) => true))
+(fact new-auction
+  (fact "defaults"
+    (let [a (new-auction {:item ...i... :bidders [1 2 3]})]
+      (instance? clojure.lang.PersistentQueue (:bidders a))
+      (= (conj clojure.lang.PersistentQueue/EMPTY 1 2 3) (:bidders a))
+      (number? (:min-increment a)) => true))
+  (fact "initial-min-bid"
+    (min-bid (new-auction {:item ...item... :bidders ...bidders...} 100)) => 100))
 
 (fact bid
-  (let [a0 (new-auction {:player-id 3 :bidders [1 2 3] :price 200})
+  (let [a0 (new-auction {:item ...i... :player-id 3 :bidders [1 2 3] :price 200})
         a1 (bid a0 1 210)]
     (:price a1) => 210
     (:player-id a1) => 1
@@ -20,7 +22,7 @@
     (current-bidder a1) => 2))
 
 (fact pass
-  (let [a0 (new-auction {:player-id 3 :bidders [1 2 3] :price 200})
+  (let [a0 (new-auction {:item ...i... :player-id 3 :bidders [1 2 3] :price 200})
         a1 (pass a0)]
     (:price a1) => 200
     (:player-id a1) => 3
@@ -30,9 +32,10 @@
     (current-bidder a1) => 2))
 
 (fact completed?
-  (let [a (pass (new-auction {:player-id 3 :bidders [1 3] :price 200}))]
+  (let [a (pass (new-auction {:item ...i... :player-id 3 :bidders [1 3] :price 200}))]
     (completed? a) => truthy))
 
 (fact min-bid
+  (min-bid (new-auction {:item ...item... :bidders ...bidders...})) => 0
   (min-bid {:price 100 :min-increment 1}) => 101
   (min-bid {:price 100 :min-increment 100}) => 200)
