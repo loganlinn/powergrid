c9e3005cb5c9351b04c98d869305dd4afb9f9b6f
loganlinn <logan.linn@gmail.com>
2013-04-21 14:10:33 -0700

    Add dijkstra base example

--- test/powergrid/cities/dijkstra_test.clj
+++ test/powergrid/cities/dijkstra_test.clj
@@ -10,18 +10,23 @@
       :c {:a 2 :b 3}})
 
 (fact dijkstra
-  (let [g (as-graph {[1 2] 7
-                     [1 3] 9
-                     [1 6] 14
-                     [2 3] 10
-                     [2 4] 15
-                     [3 4] 11
-                     [3 6] 2
-                     [4 5] 6
-                     [5 6] 9})]
-    (dijkstra g 1) => {1 0
-                       2 7
-                       3 9
-                       4 20
-                       5 20
-                       6 11}))
+  (fact "simple graph"
+    (let [g (as-graph {[:a :b] 1, [:a :c] 2, [:b :c] 3})]
+      (dijkstra g :a) => {:a 0 :b 1 :c 2}))
+
+  (fact "wikipedia example"
+    (let [g (as-graph {[1 2] 7
+                       [1 3] 9
+                       [1 6] 14
+                       [2 3] 10
+                       [2 4] 15
+                       [3 4] 11
+                       [3 6] 2
+                       [4 5] 6
+                       [5 6] 9})]
+      (dijkstra g 1) => {1 0
+                         2 7
+                         3 9
+                         4 20
+                         5 20
+                         6 11})))
