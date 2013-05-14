12ab4bad0511a8ae89d87b702be86dce53c3d5d6
Logan Linn <logan.linn@gmail.com>
2013-05-13 21:49:01 -0700

    Move cljs templates to separate file

--- src/cljs/powergrid/client.cljs
+++ src/cljs/powergrid/client.cljs
@@ -6,6 +6,7 @@
             [powergrid.common.auction :as a]
             [powergrid.common.resource :as r]
             [powergrid.common.power-plants :as pp]
+            [powergrid.client.templates :as templates]
             [dommy.template]
             [dommy.core :as dom]
             [clojure.browser.repl :as repl]
@@ -30,106 +31,10 @@
 (defn log-g [f game] (.log js/console (name f) (f game)))
 
 (set! *print-fn* log)
-(set! *remote-uri* "/funkenschlag")
 
 (def socket-bus (atom nil))
 (def game-bus (pbus/bus))
 (def current-game (atom {:id 1}))
-
-
-(defn- resource-name [r]
-  (if (set? r)
-    (clojure.string/join "-" (sort (map name r)))
-    (name r)))
-
-(def resource-el
-  {:coal [:span.resource.coal]
-   :oil  [:span.resource.oil]
-   :garbage [:span.resource.garbage]
-   :uranium [:span.resource.uranium]})
-
-(extend-type powergrid.common.player.Player
-  dommy.template/PElement
-  (-elem [{:keys [id handle color money] :as player}]
-    (letfn [(pp-rsrcs-tpl [pp-rsrcs]
-              (node [:div.power-plant-resources
-                     (mapcat
-                       #(repeat (val %) (resource-el (key %)))
-                       pp-rsrcs)]))
-            (pps-tpl [player pps]
-              (map #(vector :li (pp/plant %)
-                            (pp-rsrcs-tpl (p/power-plant-resources player %)))
-                   pps))]
-      (node [:div.player
-             {:class (str "player-" (name color))
-              :id (str "player-" id)}
-             [:span.handle handle]
-             [:div.player-icon]
-             [:div.money (str "$" money)]
-             [:ul.player-power-plants (pps-tpl player (p/power-plants player))]]))))
-
-(deftemplate player-cities-tpl [game player]
-  [:ul.player-cities
-   (map #(vector :li (name %)) (c/owned-cities (g/cities game) (p/id player)))])
-
-(extend-type powergrid.common.power-plants.PowerPlant
-  dommy.template/PElement
-  (-elem [{:keys [number resource capacity yield]}]
-    (node [:div.power-plant
-           {:class (format "%s power-plant-%d" (resource-name resource) number)}
-           [:span.number number]
-           [:span.capacity capacity]
-           [:span.yield yield]])))
-
-(deftemplate resources-tpl []
-  (let [coal-el (resource-el :coal)
-        oil-el  (resource-el :oil)
-        garbage-el (resource-el :garbage)
-        uranium-el (resource-el :uranium)]
-   [:div#resources
-   [:h3 "Resources"]
-   (for [cost (range 1 9)]
-     [:div.resource-block
-      {:data-resource-cost cost}
-      [:span.resource-block-cost cost]
-      [:div (repeat 3 coal-el)]
-      [:div (repeat 3 oil-el)]
-      [:div (repeat 3 garbage-el)]
-      [:div uranium-el]])
-   (for [cost (range 10 18 2)]
-     [:div.resource-block {:data-resource-cost cost}
-      [:span cost] [:div uranium-el]])]))
-
-(deftemplate power-plants-tpl [power-plants]
-  (let [plant-node #(node [:li (pp/plant %)])]
-    [:div#power-plants
-     [:h3 "Power Plants"]
-     [:ul.market
-      (map plant-node (:market power-plants))]
-     [:ul.future
-      (map plant-node (:future power-plants))]]))
-
-(deftemplate auction-tpl [game {:keys [item price bidders] :as auction}]
-  [:div.auction
-   [:h3 "Auction"]
-   [:div.auction-price (str "Current Bid: $" price)]
-   [:ul.auction-bidders
-    (map #(node [:li (p/handle (g/player game %))]) bidders)]
-   [:div.auction-item item]
-   [:div {:style {:clear "both"}}]])
-
-(deftemplate game-tpl [{:keys [step phase round power-plants] :as game}]
-  [:div#game
-   [:div
-    [:div (str "Step: " step)]
-    [:div (format "Phase: %d (%s)" phase (g/phase-title phase))]
-    [:div (str "Round: " round)]]
-   [:div#players
-    [:h3 "Players"]
-    (mapcat (juxt identity (partial player-cities-tpl game)) (g/players game))]
-   (resources-tpl)
-   (when-let [auction (g/auction game)] (auction-tpl game auction))
-   (power-plants-tpl power-plants)])
 
 (defn update-resource-availability
   "Applies 'unavailable' class to resources not available in market"
@@ -150,7 +55,7 @@
       (sel (str "#resources ." (name r))))))
 
 (defn render-game [game]
-  (dom/replace! (sel1 :#game) (game-tpl game))
+  (dom/replace! (sel1 :#game) (templates/game-tpl game))
   (update-resources (:resources game))
   (if-let [p (sel1 (str "#player-" (g/action-player-id game)))]
     (dom/add-class! p "has-action")))
