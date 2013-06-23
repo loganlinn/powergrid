836e7594b5b811e82bca822fe45be7ab6eeb5953
loganlinn <logan.linn@gmail.com>
2013-06-23 12:14:59 -0700

    Cleanup powergrid.message, actually call validate fn

--- src/clj/powergrid/message.clj
+++ src/clj/powergrid/message.clj
@@ -1,61 +1,64 @@
 (ns powergrid.message
   (:refer-clojure :exclude [type])
   (:require [powergrid.game :as g]
-            [powergrid.util.error :refer [fail error-m]]
+            [powergrid.util.error :refer [fail failf error-m]]
             [clojure.algo.monads :refer [with-monad m-chain]]
             [slingshot.slingshot :refer [throw+]]))
 
 (def topic :topic)
 (def type :type)
 (def pass ::pass)
+(defn is-pass? [msg] (pass msg))
 
 (defprotocol Message
   (turn? [this] "Returns true if turns should be advanced after hanlding message")
   (passable? [this game] "Returns true if passing is allowed, otherwise false")
   (update-pass [this game] "Returns (modified) game from passing msg")
-  (validate [msg game] "Retruns string of validation message if msg is invalid, otherwise nil")
-  (update-game [update game]))
+  (validate [this game] "Retruns failure if message is invalid, otherwise game")
+  (update-game [this game] "Returns game after applying valid message"))
 
-(extend-protocol Message
-  clojure.lang.IPersistentMap
-  (turn? [_] false)
-  (passable? [_ _] false)
-  (update-pass [_ game] game)
-  (validate [_ _] nil)
-  (update-game [_ game] game))
-
-(defn is-pass? [msg] (pass msg))
-
-(defn- expected-topic
-  [game]
+(defn- expected-topic [game]
   (case (g/current-phase game)
     2 :phase2
     3 :phase3
     4 :phase4
     5 :phase5))
 
-(defn base-validate
-  [{:keys [player-id] :as msg} game]
-  (cond
-    (not (g/player game player-id)) (fail "Invalid player")
-    (or (nil? (expected-topic game))
-        (not= (topic msg) (expected-topic game))) (fail "Unexpected message (phase)")
-    (and (turn? msg) (not= player-id (g/current-turn game))) (fail "Not your turn")
-    :else game))
+(defn- invalid-topic? [msg game]
+  (if (not= (expected-topic game) (topic msg))
+    (fail "Unexpected message (phase)")))
 
-(defn- validate-msg
+(defn- invalid-player? [msg game]
+  (if (not (g/player game (:player-id msg)))
+    (fail "Invalid player")))
+
+(defn- invalid-turn? [msg game]
+  (if (and (turn? msg) (not= (:player-id msg) (g/current-turn game)))
+    (failf "Not your turn (waiting for %s)" (g/current-turn game))))
+
+(defn- invalid-pass?
+  "If msg is pass, returns Failure if invalid or game if valid, otherwise nil"
   [msg game]
   (if (is-pass? msg)
     (if (passable? msg game)
-      game
-      (fail "Cannot pass"))
-    (base-validate msg game)))
+      game ; end validation
+      (fail "Cannot pass"))))
+
+(defn- validate-msg
+  "Returns a Failure if msg is invalid, otherwise a game"
+  [msg game]
+  (or
+    (invalid-pass? msg game)
+    (invalid-topic? msg game)
+    (invalid-player? msg game)
+    (invalid-turn? msg game)
+    (validate msg game)
+    (fail "Failed to validate message")))
 
 (defn- advance-turns
+  "Advance game's turns if msg is a turn"
   [msg game]
-  (if (turn? msg)
-    (g/advance-turns game)
-    game))
+  (if (turn? msg) (g/advance-turns game) game))
 
 (defn apply-message
   "Attempts to advance game state with msg. Returns [game err] tuple."
