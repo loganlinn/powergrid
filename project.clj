6de1efc39e3c8421a2799d8e09f1c21b887c064c
loganlinn <logan.linn@gmail.com>
2013-08-03 00:11:48 -0700

    Remove cljs dir
    Move to old-cljs while transitioning to pedestal

--- project.clj
+++ project.clj
@@ -11,27 +11,16 @@
                  [http-kit "2.1.1"]
                  [potemkin "0.3.0"]
                  [ring-anti-forgery "0.2.1"]
-                 [shoreleave/shoreleave-pubsub "0.3.0"]
-                 [shoreleave/shoreleave-browser "0.3.0"]
+                 [shoreleave/shoreleave-pubsub "0.3.0"] ;; TODO REMOVE
+                 [shoreleave/shoreleave-browser "0.3.0"] ;; TODO REMOVE
                  [compojure "1.1.5"]
                  [hiccup "1.0.3"]
-                 [prismatic/dommy "0.1.1"]
+                 [prismatic/dommy "0.1.1"] ;; TODO REMOVE
                  [slingshot "0.10.3"]
                  [robert/hooke "1.3.0"]
                  [midje "1.5.1"]]
-  ;:hooks [leiningen.cljsbuild]
-  :source-paths ["src/clj"]
-  :plugins  [[lein-cljsbuild "0.3.0"]
-             [lein-ring "0.8.5"]]
+  :plugins  []
   :profiles {:dev {:source-paths  ["dev"]
                    :dependencies [[org.clojure/tools.namespace "0.2.3"]
                                   [org.clojure/java.classpath "0.2.0"]]
-                   :plugins [[lein-midje "3.0.0"]]}}
-  :cljsbuild {:crossovers [powergrid.common]
-              :builds [{:id "main"
-                        :source-paths ["src/cljs"]
-                        :crossover-path "common-cljs"
-                        :compiler {:output-to "resources/public/js/cljs.js"
-                                   :optimizations :simple
-                                   :pretty-print true}
-                        :jar true}]})
+                   :plugins [[lein-midje "3.0.0"]]}})
